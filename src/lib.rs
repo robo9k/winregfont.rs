cdc6c58bf46a5d1ef62a3398ea770c35cffbb8c3
robot9001 <robo9k@symlink.io>
2021-04-01 12:15:30 -0700

    Limit unsafe block further

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@ pub mod font_resource {
 
     pub fn add_from_cstring<F: Into<widestring::U16CString>>(filename: F) -> Result<u16, ()> {
         let mut filename = filename.into().into_vec_with_nul();
-        let filename = filename.as_mut_ptr();
+        let filename = PWSTR(filename.as_mut_ptr());
 
-        match unsafe { AddFontResourceW(PWSTR(filename)) } {
+        match unsafe { AddFontResourceW(filename) } {
             0 => Err(()),
             rv => Ok(u16::try_from(rv).expect("API contract violation")),
         }
