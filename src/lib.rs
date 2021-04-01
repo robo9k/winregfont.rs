40673f0084a5bb033fd9fde4fe0b345a0c4d3658
robot9001 <robo9k@symlink.io>
2021-04-01 12:08:38 -0700

    Split code into bin and lib, use `widestring` for Windows `PWSTR` FFI

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub mod font_resource {
+    use bindings::Windows::Win32::{Gdi::AddFontResourceW, SystemServices::PWSTR};
+    use std::convert::TryFrom;
+
+    pub fn add_from_cstring<F: Into<widestring::U16CString>>(filename: F) -> Result<u16, ()> {
+        let mut filename = filename.into().into_vec_with_nul();
+        let filename = filename.as_mut_ptr();
+
+        match unsafe { AddFontResourceW(PWSTR(filename)) } {
+            0 => Err(()),
+            rv => Ok(u16::try_from(rv).expect("API contract violation")),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-use bindings::Windows::Win32::{Gdi::AddFontResourceW, SystemServices::PWSTR};
-use std::iter::once;
-use std::os::windows::prelude::*;
+use winregfont::font_resource::add_from_cstring as add_font_resource;
 
 fn main() {
     let filename = std::env::args_os()
         .nth(1)
         .expect("Expected one command line argument for font filename");
-    let mut filename: Vec<u16> = filename.encode_wide().chain(once(0)).collect();
+    let filename =
+        widestring::U16CString::from_os_str(filename).expect("Filename arg contains NUL values");
 
-    let rv = unsafe {
-        let filename = filename.as_mut_ptr();
-        AddFontResourceW(PWSTR(filename))
-    };
-
-    println!("AddFontResourceW: {}", rv);
+    let num_fonts_added = add_font_resource(filename).expect("Success adding font resource");
+    println!("AddFontResourceW: {}", num_fonts_added);
 }
